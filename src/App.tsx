1246e24891d0f005f92b949a037b601cf25d1e9e
Pedro Paulo <pedropaulo@interlinksistemas.com.br>
2026-03-24 16:00:48 -0300

    feat: Header incluido já com responsivo e ajustes entre itens

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { Header } from "./header/header";
 function App() {
-  return <main className="bg-color min-h-screen"></main>;
+  return (
+    <main className="min-h-screen flex flex-col bg-bg-main">
+      <Header />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/header/header.tsx
@@ -0,0 +1,54 @@
+import listIcon from "../assets/icons/List.svg";
+import logo from "../assets/logo.svg";
+import search from "../assets/icons/MagnifyingGlass.svg";
+
+export function Header() {
+  return (
+    <header className="">
+      <nav
+        id="Primary"
+        className="grid grid-cols-[1fr_auto_1fr] items-center px-4 py-2"
+      >
+        <div className="grid grid-flow-col auto-cols-max items-center gap-2 justify-self-start">
+          <img src={listIcon} alt="List" className="h-4 sm:h-6 md:h-8 w-auto" />
+          <h2 className="text-text-primary">menu</h2>
+        </div>
+
+        <div className="justify-self-center">
+          <img src={logo} alt="Logo" className="h-8 w-auto" />
+        </div>
+        <div className="grid grid-flow-col auto-cols-max items-center gap-2 justify-self-end">
+          <h2 className="text-text-primary">buscar</h2>
+          <img src={search} alt="List" className="h-4 sm:h-6 md:h-8 w-auto" />
+        </div>
+      </nav>
+
+      <nav
+        id="Secondary"
+        className="text-text-primary grid grid-cols-2 py-[14px] px-[32px] gap-4 md:grid-cols-7 items-center justify-items-center text-center border-y border-gray-700"
+      >
+        <a href="#" className="hover:text-brand-light">
+          Inteligencia Arttificial
+        </a>
+        <a href="#" className="hover:text-brand-light">
+          Blockchain
+        </a>
+        <a href="#" className="hover:text-brand-light">
+          Hologramas
+        </a>
+        <a href="#" className="hover:text-brand-light">
+          Internet
+        </a>
+        <a href="#" className="hover:text-brand-light">
+          Vestíveis
+        </a>
+        <a href="#" className="hover:text-brand-light">
+          Realidade Aumentada
+        </a>
+        <a href="#" className="hover:text-brand-light">
+          Realidade Virtual
+        </a>
+      </nav>
+    </header>
+  );
+}
