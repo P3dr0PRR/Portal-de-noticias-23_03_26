b3b8a062e0d0f2812cd072fa754b5d5ddead5ade
Pedro Paulo <pedropaulo@interlinksistemas.com.br>
2026-03-24 16:05:26 -0300

    feat: fix: correção do import da logo

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -1,5 +1,5 @@
 import listIcon from "../assets/icons/List.svg";
-import logo from "../assets/logo.svg";
+import logo from "../assets/Logo.svg";
 import search from "../assets/icons/MagnifyingGlass.svg";
 
 export function Header() {
